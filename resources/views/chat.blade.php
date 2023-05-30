3d2ed64916869e8bec2b67864a0c594604584574
pniyonshuti <prince.niyonshuti@zatec.io>
2023-05-30 10:08:50 +0200

    init data

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        .typing-animation {
+            animation: typing 3s steps(40) 1s forwards;
+        }
+
+        @keyframes typing {
+            from {
+                width: 0;
+            }
+
+            to {
+                width: 100%;
+            }
+        }
+    </style>
+
+
+</head>
+
+<body>
+    <header class="bg-white shadow">
+        <div class="container mx-auto px-4 py-3">
+            <nav class="flex items-center justify-between">
+                <div>
+                    <a href="#" class="text-gray-800 font-bold text-lg">Zatec - OpenAi Challenge</a>
+                </div>
+                <div>
+                    <a href="/" class="text-gray-600 hover:text-gray-800">Home</a>
+                    <a href="/" class="text-gray-600 hover:text-gray-800 ml-4">Questions</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main class="container mx-auto px-4 py-8">
+        <section class="bg-white rounded-lg shadow-lg p-6">
+            <h1 class="text-2xl font-bold mb-4">Coding Challenge</h1>
+            <h2 class="text-lg font-bold mb-2">Problem Statement</h2>
+            <p class="text-gray-700 mb-6">
+                <q>
+                    write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included)
+                </q>
+            </p>
+            <div class="border-t border-gray-300 pt-6">
+                <h2 class="text-lg font-bold mb-2">Your Solution</h2>
+                <form method="post" action="/process">
+                    @csrf
+                    <div class="bg-gray-100 p-4 rounded-lg mb-6">
+                        <textarea id="user-input" name="user_input"
+                            class="w-full h-32 p-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                            placeholder="Write your solution here..."></textarea>
+                    </div>
+                    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600">Submit</button>
+                </form>
+                <h2 class="text-lg font-bold mb-2 mt-6">Answer Verification</h2>
+                <div class="bg-gray-100 p-4 rounded-lg mb-6">
+                    <code class="text-gray-800">
+                        <div id="typing-effect">
+                            @if(isset($generatedText))
+                                <p>{{ $generatedText }}</p>
+                            @endif
+                        </div>
+                    </code>
+                </div>
+            </div>
+        </section>
+    </main>
+    <script>
+        @if(isset($generatedText))
+            const text = {!! json_encode($generatedText) !!};
+                const typingEffect = document.getElementById("typing-effect");
+                function typeText() {
+                typingEffect.innerHTML = '';
+                typingEffect.classList.add('typing-animation');
+                let i = 0;
+                const typingInterval = setInterval(() => {
+                    typingEffect.innerHTML += text[i];
+                    i++;
+                    if (i === text.length) {
+                    clearInterval(typingInterval);
+                    }
+                }, 80);
+            }
+            typeText();
+        @endif
+    </script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('chat');
+});
+
+Route::post('/process', function () {
+    // $userInput = request('user_input');
+    $userAnswer = request('user_input');
+
+    $constant1 = "Suppose you were a code reviewer, and you check an applicant's code with the following algorithm question asking to";
+    $dynamicVariable1 = "write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included).";
+    $constant2 = "point out in bulleted points the good points and also the needed improvements, afterwards provide in one word if this person is a junior, mid-level or senior developer. The applicant's answer is:";
+    $dynamicVariable2 = $userAnswer;
+
+    $userInput = $constant1 . " " . $dynamicVariable1 . " " . $constant2 . " " . $dynamicVariable2;
+
+
+    $apiKey = "your-api-key"; // Replace with your actual API key
+
+    $headers = [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $apiKey,
+    ];
+
+    $data = [
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            ['role' => 'system', 'content' => 'You are a helpful assistant.'],
+            ['role' => 'user', 'content' => $userInput],
+        ],
+    ];
+
+    $dataString = json_encode($data);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/chat/completions');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $output = json_decode($response, true);
+    $generatedText = $output['choices'][0]['message']['content'];
+
+    return view('chat')->with('generatedText', $generatedText);
+});
